f7dc888b03da9aa4ad7b189b28387f73f78ad9c6
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2021-01-10 09:21:44 +0000

    Adding world.rs

---
+++ src/world.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct World {
+	grid: Grid
+}
+
+impl World {
+	pub fun new(grid: Grid) -> World {
+		World { grid }
+	}
+
+	pub fn next(&self) -> World {
+		
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn empty_grid() {
+    //     let grid = Grid::new(0, 0);
+
+    //     assert_eq!(grid.cells.len(), 0);
+    // }
+}
