7fbf1fe063362ea47d7aba97b068938a951a3b8b
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2021-01-14 08:34:33 +0000

    test edges for neightbours

--- src/world.rs
+++ src/world.rs
@@ -94,6 +94,12 @@ mod tests {
         assert_eq!(world, updated_world);
     }
 
+    /*
+
+       * *
+       * *
+
+    */
     #[test]
     fn find_all_neighbours() {
         let grid = Grid::new(2, 2);
@@ -110,6 +116,14 @@ mod tests {
         assert_eq!(actual_neighbours, expected_neightbours);
     }
 
+    /*
+
+       * . . .
+       . . . .
+       . . . .
+       . . . .
+
+    */
     #[test]
     fn find_neighbours_top_left_corner() {
         let grid = Grid::new(4, 4);
@@ -126,6 +140,20 @@ mod tests {
         assert_eq!(actual_neighbours, expected_neightbours);
     }
 
+    /*
+
+       . . . . . . . . . .
+       . . . . . . . . . .
+       . . * . . . . . . .
+       . . . . . . . . . .
+       . . . . . . . . . .
+       . . . . . . . . . .
+       . . . . . . . . . .
+       . . . . . . . . . .
+       . . . . . . . . . .
+       . . . . . . . . . .
+
+    */
     #[test]
     fn find_neighbours_in_centre() {
         let grid = Grid::new(10, 10);
@@ -156,6 +184,14 @@ mod tests {
         assert_eq!(actual_neighbours, expected_neightbours);
     }
 
+    /*
+
+       . . . .
+       . . . .
+       . . . .
+       . . . *
+
+    */
     #[test]
     fn find_neighbours_bottom_right_corner() {
         let grid = Grid::new(4, 4);
@@ -168,6 +204,134 @@ mod tests {
         let expected_neightbours = vec![above_left_cell, left_cell, above_cell];
 
         let actual_neighbours = world.neighbours(&world.grid.cells[3][3]);
+
+        assert_eq!(actual_neighbours, expected_neightbours);
+    }
+
+    /*
+
+       . . . .
+       * . . .
+       . . . .
+       . . . .
+
+    */
+    #[test]
+    fn find_neighbours_left_edge() {
+        let grid = Grid::new(4, 4);
+        let world = World::new(grid);
+
+        let above_cell = &Cell::new(0, 0);
+        let below_cell = &Cell::new(0, 2);
+        let above_right_cell = &Cell::new(1, 0);
+        let right_cell = &Cell::new(1, 1);
+        let below_right_cell = &Cell::new(1, 2);
+
+        let expected_neightbours = vec![
+            above_cell,
+            below_cell,
+            above_right_cell,
+            right_cell,
+            below_right_cell,
+        ];
+
+        let actual_neighbours = world.neighbours(&world.grid.cells[1][0]);
+
+        assert_eq!(actual_neighbours, expected_neightbours);
+    }
+
+    /*
+
+       . . . .
+       . . . .
+       . . . *
+       . . . .
+
+    */
+    #[test]
+    fn find_neighbours_right_edge() {
+        let grid = Grid::new(4, 4);
+        let world = World::new(grid);
+
+        let above_left_cell = &Cell::new(2, 1);
+        let left_cell = &Cell::new(2, 2);
+        let below_left_cell = &Cell::new(2, 3);
+        let above_cell = &Cell::new(3, 1);
+        let below_cell = &Cell::new(3, 3);
+
+        let expected_neightbours = vec![
+            above_left_cell,
+            left_cell,
+            below_left_cell,
+            above_cell,
+            below_cell,
+        ];
+
+        let actual_neighbours = world.neighbours(&world.grid.cells[2][3]);
+
+        assert_eq!(actual_neighbours, expected_neightbours);
+    }
+
+    /*
+
+       . . * .
+       . . . .
+       . . . .
+       . . . .
+
+    */
+    #[test]
+    fn find_neighbours_above_edge() {
+        let grid = Grid::new(4, 4);
+        let world = World::new(grid);
+
+        let left_cell = &Cell::new(1, 0);
+        let below_left_cell = &Cell::new(1, 1);
+        let below_cell = &Cell::new(2, 1);
+        let right_cell = &Cell::new(3, 0);
+        let below_right_cell = &Cell::new(3, 1);
+
+        let expected_neightbours = vec![
+            left_cell,
+            below_left_cell,
+            below_cell,
+            right_cell,
+            below_right_cell,
+        ];
+
+        let actual_neighbours = world.neighbours(&world.grid.cells[0][2]);
+
+        assert_eq!(actual_neighbours, expected_neightbours);
+    }
+
+    /*
+
+       . . . .
+       . . . .
+       . . . .
+       . * . .
+
+    */
+    #[test]
+    fn find_neighbours_below_edge() {
+        let grid = Grid::new(4, 4);
+        let world = World::new(grid);
+
+        let above_left_cell = &Cell::new(0, 2);
+        let left_cell = &Cell::new(0, 3);
+        let aobve_cell = &Cell::new(1, 2);
+        let above_right_cell = &Cell::new(2, 2);
+        let right_cell = &Cell::new(2, 3);
+
+        let expected_neightbours = vec![
+            above_left_cell,
+            left_cell,
+            aobve_cell,
+            above_right_cell,
+            right_cell,
+        ];
+
+        let actual_neighbours = world.neighbours(&world.grid.cells[3][1]);
 
         assert_eq!(actual_neighbours, expected_neightbours);
     }
