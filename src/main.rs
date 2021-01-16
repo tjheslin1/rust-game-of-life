783d8fd21122e59a2048e450ca9dffb74dbc9fd5
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2021-01-16 08:31:45 +0000

    adding spaceship config

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,23 @@ use world::World;
 
 #[rustfmt::skip]
 fn main() {
-    let mut world = World { grid: Grid::new_alive_grid(40, 40, 
+ //    let mut world = World { grid: Grid::new_alive_grid(40, 40, 
+	//     	vec![
+	//     		(3, 2),
+	//     		(3, 4), //(4, 4),
+	//     		(3, 5),
+	//     		(3, 6), (4, 6), (6, 7),
+	//     		(3, 8), (4, 8), (6, 8),
+	//     	]
+	// 	)
+	// };
+
+	// spaceship
+	let mut world = World { grid: Grid::new_alive_grid(40, 40, 
 	    	vec![
-	    		(3, 2),
-	    		(3, 4), //(4, 4),
+	    		(2, 3), (4, 3),
+	    		(3, 4), (4, 4),
 	    		(3, 5),
-	    		(3, 6), (4, 6), (6, 7),
-	    		(3, 8), (4, 8), (6, 8),
 	    	]
 		)
 	};
