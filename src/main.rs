790bf0a0a402a7b9bde581c4ff85d6bd273f261a
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2021-01-17 13:33:24 +0000

    adding default random seed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::cmp;
 use std::{thread, time};
 use rand::{Rng,SeedableRng};
@@ -10,22 +11,39 @@ mod world;
 use grid::Grid;
 use world::World;
 
+/*
+	memorable seeds:
+	- 43 (starting cells = 40)
+	- 4045 (starting cells = 40)
+	- 3658 (starting cells = 40)
+*/
 #[rustfmt::skip]
 fn main() {
 	let width = 40;
 	let height = 40;
 	let num_starting_cells = 40;
-	let seed = 43;
+    let random_seed: u32 = rand::thread_rng().gen_range(1, 10000);
 
-	println!("seed = {}", seed);
+	let mut input_seed = String::new();
+	println!("Input a seed or leave blank for random:");
+	io::stdin()
+		.read_line(&mut input_seed)
+		.expect("Failed to read line");
+
+	let seed: u64 = match input_seed.trim().parse() {
+		Ok(user_seed) => user_seed,
+		Err(_) => random_seed.into(),
+	};
 
 	let mut rng = StdRng::seed_from_u64(seed);
+
+	println!("seed = {}", seed);
 
     let mut live_cells: Vec<(u32, u32)> = vec![];
     for _ in 1..num_starting_cells {
     	live_cells.push(
     		(
-    			rng.gen_range(cmp::max(0, (width/2)-10), cmp::min(width, (width/2)+10)),
+				rng.gen_range(cmp::max(0, (width/2)-10), cmp::min(width, (width/2)+10)),
     			rng.gen_range(cmp::max(0, (height/2)-10), cmp::min(height, (height/2)+10))
 			)
 		);
@@ -48,10 +66,10 @@ fn main() {
 	// 	)
 	// };
 
-    let five_hundred_millis = time::Duration::from_millis(500);
+    let five_hundred_millis = time::Duration::from_millis(250);
 
     print!("\x1B[2J\x1B[1;1H");
-    for i in 1..100 {
+    for i in 1..1000 {
         println!("{}: seed = {}", i, seed);
         print!("{}", world.grid.display());
 
