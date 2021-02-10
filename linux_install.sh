0ce3e4651a6a1409ec4d7515fb7494ce2b6ff998
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2021-02-10 06:47:25 +0000

    install script update

--- linux_install.sh
+++ linux_install.sh
@@ -1,20 +1,22 @@
-GOL_HOLE_INSTALL_DIR="/opt/game_of_life"
-GOL_HOLE_BIN_DIR="/usr/local/bin"
+GOL_INSTALL_DIR="/opt/game_of_life"
+GOL_BIN_DIR="/usr/local/bin"
 
 if [ -z "${USER}" ]; then
   USER="$(id -un)"
 fi
 
-echo "cleaning up /opt/game_of_life"
-rm -rf "${GOL_HOLE_INSTALL_DIR}"
+install -o "${USER}" -Dm755 -d "${GOL_INSTALL_DIR}"
 
-echo "Shallow clone of tjheslin1/rust-game-of-life to ${GOL_HOLE_INSTALL_DIR}"
-mkdir -p "${GOL_HOLE_INSTALL_DIR}"
-cd "${GOL_HOLE_INSTALL_DIR}"
+# echo "cleaning up /opt/game_of_life"
+# rm -rf "${GOL_INSTALL_DIR}"
+
+echo "Shallow clone of tjheslin1/rust-game-of-life to ${GOL_INSTALL_DIR}"
+# mkdir -p "${GOL_INSTALL_DIR}"
+cd "${GOL_INSTALL_DIR}"
 git clone --depth 1 https://github.com/tjheslin1/rust-game-of-life.git 1>/dev/null 2>&1
 
 echo "Copying binary to /usr/local/bin/gol"
-mv "${GOL_HOLE_INSTALL_DIR}"/rust-game-of-life/release/game_of_life "${GOL_HOLE_INSTALL_DIR}"/rust-game-of-life/release/gol
-chmod +x "${GOL_HOLE_INSTALL_DIR}"/rust-game-of-life/release/gol
+mv "${GOL_INSTALL_DIR}"/rust-game-of-life/release/game_of_life "${GOL_INSTALL_DIR}"/rust-game-of-life/release/gol
+chmod +x "${GOL_INSTALL_DIR}"/rust-game-of-life/release/gol
 # cp /opt/game_of_life/rust-game-of-life/release/game_of_life /usr/local/bin/gol
-install -o "${USER}" -Dm755 -t "${GOL_HOLE_BIN_DIR}" "${GOL_HOLE_INSTALL_DIR}"/rust-game-of-life/release/gol
+install -o "${USER}" -Dm755 -t "${GOL_BIN_DIR}" "${GOL_INSTALL_DIR}"/rust-game-of-life/release/gol
