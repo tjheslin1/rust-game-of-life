1068a697becaaeb2e0dfecb4451ee4ca76f19617
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2022-03-20 10:07:34 +0000

    new file: src/brians_brain.rs

---
+++ src/brians_brain.rs
@@ -0,0 +1,40 @@
+use crate::cell::Cell;
+use crate::game::Game;
+use crate::grid::Grid;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct BriansBrain {
+    pub grid: Grid,
+    pub seed: u32,
+}
+
+impl Game<BriansBrain> for BriansBrain {
+    fn next(&self) -> BriansBrain {}
+}
+
+impl BriansBrain {
+    pub fn is_alive(cell: &Cell, neighbours: Vec<&Cell>) -> bool {
+        unimplemented!("todo")
+    }
+
+    pub fn neighbours(&self, cell: &Cell) -> Vec<&Cell> {
+        unimplemented!("todo")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::game::Game;
+
+    #[test]
+    fn world_preserves_seed() {
+        let grid = Grid::new(1, 1);
+        let world = BriansBrain { grid, seed: 55 };
+
+        let updated_world = world.next();
+
+        assert_eq!(updated_world.seed, 55);
+    }
+}
