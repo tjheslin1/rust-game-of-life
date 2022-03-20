9d929aa95c71610e348885b2f4a0b881b96ed630
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2022-03-20 15:55:34 +0000

    Update release process

--- linux_install.sh
+++ linux_install.sh
@@ -8,12 +8,12 @@ fi
 sudo install -o "${USER}" -Dm755 -d "${GOL_INSTALL_DIR}"
 
 echo "Shallow clone of tjheslin1/rust-game-of-life to ${GOL_INSTALL_DIR}"
+
 cd "${GOL_INSTALL_DIR}"
+
 git clone -q --depth 1 https://github.com/tjheslin1/rust-game-of-life.git 1>/dev/null 2>&1
-# chown -R "${USER}" 
+# chown -R "${USER}"
 
 echo "Copying binary to /usr/local/bin/gol"
-
-mv "${GOL_INSTALL_DIR}"/rust-game-of-life/release/game_of_life "${GOL_INSTALL_DIR}"/rust-game-of-life/release/gol
 
 sudo install -o "${USER}" -Dm755 -t "${GOL_BIN_DIR}" "${GOL_INSTALL_DIR}"/rust-game-of-life/release/gol
